e0565f41e02b42e6123b6d4df0aedbb4b3fcab98
MHTNY Security <mhtny.security@protonmail.com>
2017-06-12 10:41:54 -0400

    Add new Author model, create relationship between Book and Author

--- books/admin.py
+++ books/admin.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-from .models import Book
+from .models import Author, Book
 # Register your models here.
+admin.site.register(Author)
 admin.site.register(Book)
