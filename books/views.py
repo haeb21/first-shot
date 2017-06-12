ea4974d972f6ea57bb5f5efee893f48dc098885f
MHTNY Security <mhtny.security@protonmail.com>
2017-06-12 15:00:22 -0400

    Add list of authors, author page, and book page

--- books/views.py
+++ books/views.py
@@ -2,7 +2,7 @@
 from __future__ import unicode_literals
 from django.db.models import Count
 from django.shortcuts import render
-from django.views.generic import View
+from django.views.generic import DetailView, View
 from .models import Author, Book
 
 # Create your views here.
@@ -33,3 +33,13 @@ class AuthorList(View):
 		}
 		
 		return render(request, "authors.html", context)
+		
+		
+class BookDetail(DetailView):
+	model = Book
+	template_name = "book.html"
+	
+
+class AuthorDetail(DetailView):
+	model = Author
+	template_name = "author.html"
