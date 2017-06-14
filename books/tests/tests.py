245efe4aff8f44c7aecc1f9452bfee68996c4490
MHTNY Security <mhtny.security@protonmail.com>
2017-06-14 12:12:21 -0400

    Add demo test, create factories

--- books/tests/tests.py
+++ books/tests/tests.py
@@ -4,3 +4,6 @@ from __future__ import unicode_literals
 from django.test import TestCase
 
 # Create your tests here.
+class DemoTest(TestCase):
+	def test_addition(self):
+		self.assertEqual(1 + 1, 4)
